526629939df54d1ea55562f1c7b27fae23c2a6d5
Siegfried Eckstedt <siegfriedeckstedt@gmail.com>
2022-11-04 11:11:18 +0100

    extended on learnings

--- src/converters.py
+++ src/converters.py
@@ -1,13 +1,12 @@
-def convert_to_int(integer_string_with_commas: str) -> int:
-    comma_separated_parts = integer_string_with_commas.split(",")
+def convert_to_int(integer_with_commas: str) -> int:
+    comma_separated_parts = integer_with_commas.split(",")
     for i in range(len(comma_separated_parts)):
         if len(comma_separated_parts[i]) > 3:
             return None
         if i != 0 and len(comma_separated_parts[i]) != 3:
             return None
-
-    integer_string_with_commas = "".join(comma_separated_parts)
+    
     try:
-        return int(integer_string_with_commas)
+        return int(integer_with_commas.replace(",", ""))
     except ValueError:
         return None
